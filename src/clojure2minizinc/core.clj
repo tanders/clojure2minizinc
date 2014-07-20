849474b78fbe1a351329dcbcf4ce5fd463a4710c
Torsten Anders <torsten.anders@beds.ac.uk>
2014-07-20 21:15:44 +0100

    Fixed doc bug of minizinc

--- src/clojure2minizinc/core.clj
+++ src/clojure2minizinc/core.clj
@@ -1007,7 +1007,7 @@ Options are
 
 :mzn            (string) a MiniZinc program, which can be created with other functions of clojure2minizinc wrapped into clj2mnz
 :print-mzn?     (boolean) whether or not to print resulting MiniZinc program (for debugging)
-:print-mzn?     (boolean) whether or not to print the result output by MiniZinc directly instead of reading it into a Clojure value (e.g., for debugging). Prints the map resulting from clojure.java.shell/sh.
+:print-solution?     (boolean) whether or not to print the result output by MiniZinc directly instead of reading it into a Clojure value (e.g., for debugging). Prints the map resulting from clojure.java.shell/sh.
 :solver         (string) solver to call
 :mznfile        (string or file) MiniZinc file path to generate and use in the background
 :data           (string) Content for a MiniZinc data file (*.dzn file). Can conveniently be created with map2minizinc 
