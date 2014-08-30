6f43abc57080d347a1b21b9101a1580c2554d603
Torsten Anders <torsten.anders@beds.ac.uk>
2014-08-30 23:50:01 +0100

    Project file with group (and other minor edits)

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject clojure2minizinc "0.1.0-SNAPSHOT"
+(defproject minizinc/clojure2minizinc "0.1.0-SNAPSHOT"
   :description "This library provides an interface to the MiniZinc constraint modelling language (http://www.minizinc.org/), and constraint solvers supporting a FlatZinc interface (e.g., Gecode, http://www.gecode.org/flatzinc.html)."
   ;; :url "http://example.com/FIXME"
   :license {:name "GNU General Public License"
             :url "https://gnu.org/licenses/gpl.html"}
-  ;; TODO: add dependencies -- how can Leiningen add dependencies outside Clojure?
-  ;; mzn2fzn as part of http://www.minizinc.org/g12distrib.html -- note that this distribution additionally needs to run and install script locally
-  ;; Gecode/FlatZinc, see http://www.gecode.org/flatzinc.html
-  ;; or other FlatZinc implementations
+  ;; ?? TODO: add dependencies -- how can Leiningen add dependencies outside Clojure?
+  ;; Possible solution: https://github.com/pliant/lein-package
+  ;; Difficulties: 
+  ;; - The MiniZinc distribution and solvers are platform specific, so I would need an extra project for each platform.
+  ;; - The MiniZinc distribution installation requires running some install script locally
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [me.raynes/fs "1.4.5"]
                  [org.clojure/math.combinatorics "0.0.7"]
