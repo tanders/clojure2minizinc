089c6909c09ba9e0cf42a0a480d4d36c65fe3668
Torsten Anders <torsten.anders@beds.ac.uk>
2016-06-28 11:32:53 +0100

    Added dummy example tmp file

---
+++ doc/example-drafts/tmp.clj
@@ -0,0 +1,9 @@
+(ns clojure2minizinc.tmp
+  (:require [clojure2minizinc.core :as mz] ; 
+            ;; [cplojure.java.shell :as shell]
+            ;; [clojure.pprint :as pprint]
+            )
+  )
+
+
+
